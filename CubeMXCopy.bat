89018cb0d523d419f4c75bbc6b15098d2776b906
Sebastian Gondek <104504196+sebgone@users.noreply.github.com>
2024-05-16 20:47:03 +0200

    [1] Initial commit. Setting repository. First version of script

---
+++ CubeMXCopy.bat
@@ -0,0 +1,72 @@
+@echo off
+REM This is a script for automatic copy project in MXcube IDE
+
+setlocal
+
+REM Change the directory to the specified folder
+REM cd "path to workspace directory"
+
+REM store arguments in the variables
+REM quotation marks prevent and handling spaces in direcotry names
+set "old_project_name=%~1"
+set "new_project_name=%~2"
+
+if exist "%new_project_name%" (
+
+    echo Project name already exist.
+    echo Change name and try again.
+    exit /b
+)
+
+REM check if first argument "project directory you want to copy" exist
+if exist "%old_project_name%" (
+
+    echo Project directory found: "%old_project_name%"
+    
+REM redirects output from xcopy command to >NUL, suppress output    
+    xcopy /E /I "%old_project_name%" "%old_project_name%_copy" >NUL
+    echo Project copied
+    
+REM rename project and go inside
+    ren "%old_project_name%_copy" "%new_project_name%"
+    echo Copied project renamed.
+    cd "%new_project_name%"
+
+REM if exist delete debug configuration files
+    if exist "%old_project_name% Debug.launch" (
+        del "%old_project_name% Debug.launch"
+        echo Debug file deleted.
+    ) else (
+        echo No Debug files.
+    )
+    
+REM Rename .ioc project file
+    if exist "%old_project_name%.ioc" (
+        ren "%old_project_name%.ioc" "%new_project_name%.ioc"
+        echo .ioc file renamed.
+        
+    ) else (
+        echo ioc file missing.
+    )
+    
+REM Replace old project names in .cproject and .project files
+REM Finding and replacing string by powershell command
+    if exist ".cproject" (
+        powershell -Command "(Get-Content '.cproject') -replace '%old_project_name%', '%new_project_name%' | Set-Content '.cproject'"
+        echo .cproject changed.
+    ) else (
+        echo .cproject file is missing.
+    )
+    
+    if exist ".project" (
+        powershell -Command "(Get-Content '.project') -replace '%old_project_name%', '%new_project_name%' | Set-Content '.project'"
+        echo .project changed.
+    ) else (
+        echo .project file is missing.
+    )
+    
+) else (
+    echo Project not found.
+)
+
+endlocal
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Steps for copy projects automatically:
+
+1. Move script to your CubeMX workspace directory.
+2. Open cmd in that directory.
+3. Run the script with two arguments: "name of existing project" "name of new project".
+   ex: CubeMXCopy "example directory" "new directory".
+4. Import new project directory by CubeMX IDE.
+5. Continue working on your new instance of a project.   
